9ff8f3758c246b9a956320c1b0dee6166fe653d0
Eric Oestrich <eric@oestrich.org>
2010-11-19 20:30:20 -0500

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshare::Application.config.secret_token = 'cbe0bf1ca1fdc12c1b69266dfe13865279c1541bddd5c74de06a52465adf80bd90899e19d3c26fc1967859a6da1dd2355beed68cb53af8a540adbdb08190d6b5'
