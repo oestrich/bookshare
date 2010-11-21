6b73e0f43af972f16fab9e03700940e356a822ec
Patricia <gorla.patricia@gmail.com>
2010-11-21 13:47:26 -0500

    redirect to / after new loc creation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -47,11 +47,12 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to(@location, :notice => 'Location was successfully created.') }
+        format.html { redirect_to('/', :notice => 'Location was successfully created.') }
         format.xml  { render :xml => @location, :status => :created, :location => @location }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+        redirect_to "/locations/new"
       end
     end
   end
