717d952cb83d98aa8ec6b94ed2df2d78259a5ff2
Eric Oestrich <eric@oestrich.org>
2010-11-21 14:45:45 -0500

    Changed email message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
   default :from => "team@bookshare.me"
 
   def borrow_book_confirmation(book, borrower)
-    mail(:to => book.user.email, :subject => "#{borrower.email} would like to borrow #{book.title}", :from => "team@bookshare.me")
+    mail(:to => book.user.email, :subject => "#{borrower.email} borrowed #{book.title}", :from => "team@bookshare.me")
   end
 end
