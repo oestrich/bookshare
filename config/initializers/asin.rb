1325e3159b0bc1a344ca4db93f4355baf26b3632
Jeff Casimir <jeff@casimircreative.com>
2010-12-12 22:49:32 -0500

    No longer necessary and didn't work anyway

--- config/initializers/asin.rb
+++
@@ -1,6 +0,0 @@
-require 'asin'
-include ASIN
-
-# use the configure method to setup your api credentials
-configure :secret => 'G6aSVuRonDppc5l1U10dRZW360LL+b3khm/9G6q9', :key => '0HFCVPEF9DTHP5DXV482'
-
