a3b7752e86f7a8e838470294f6e99c61d046f86a
Jeff Casimir <jeff@casimircreative.com>
2010-12-12 22:48:54 -0500

    Stripped CONVERT from the helpers and pushed it down to the model level in LookupService

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,31 +1,6 @@
 module BooksHelper
 
 
-# loosely based on http://snipplr.com/view/13299/convert-upc-to-isbn/
-# returns ISBN-10
-
-  def convert(upc)
-      """Returns isbn string"""
-      isbn = upc[3..-2]
-      add = 0
-      count = 10
-      index = 0
-      while index < isbn.length():
-          a = isbn[index..index]
-          add += a.to_i() * count
-          index += 1
-          count -= 1
-      end
-      xsum = 11 - (add % 11)
-      if xsum == 10
-          xsum = "X"
-      elsif xsum == 11
-          xsum = 0
-      end
-      isbn += xsum.to_s()
-      return isbn
-  end
-
 end
 
 #begin
