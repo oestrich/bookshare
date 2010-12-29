1b47f655f1ea8dfb629e28ec7117b23762593798
Shea Frederick <justarock@gmail.com>
2010-12-28 21:20:35 -0500

    filtering

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+document.observe("dom:loaded", function() {  
+  $('filter').observe('keyup',function(ev){
+    if (ev.srcElement.value) {
+      $$('p.title').each(function(v,i){
+        if (new RegExp('.*'+ev.srcElement.value,'ig').test(v.innerHTML)){
+          v.up().show();
+        }else{
+          v.up().hide();
+        }
+      });
+    }else{
+      $$('div.book').each(function(v,i){
+        v.show();
+      });
+    }
+  });
+});
