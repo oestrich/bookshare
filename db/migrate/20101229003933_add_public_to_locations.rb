e8306d273412046793d4f1e1894c80933765bd4e
Eric Oestrich <eric@oestrich.org>
2011-01-03 15:12:40 -0500

    Pushing to heroku caused a sql error

--- db/migrate/20101229003933_add_public_to_locations.rb
+++ db/migrate/20101229003933_add_public_to_locations.rb
@@ -1,6 +1,6 @@
 class AddPublicToLocations < ActiveRecord::Migration
   def self.up
-    add_column :locations, :public, :boolean, :default => 0
+    add_column :locations, :public, :boolean, :default => false
   end
 
   def self.down
