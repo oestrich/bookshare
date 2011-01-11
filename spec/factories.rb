f12d89fd4fc807f79699da6cd99dc99cca51f7e5
Eric Oestrich <eric@oestrich.org>
2011-01-11 10:22:45 -0500

    Factories use assocations

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,23 +1,25 @@
 
 Factory.define :user do |f|
   f.first_name "Test"
-  f.last_name "User"
+  f.sequence(:last_name) { |n| "User #{n}" }
   f.sequence(:email) { |n| "example-#{n}@example.com" }
   f.password "test123"
   f.password_confirmation "test123"
 end
 
 Factory.define :book do |f|
-  f.title "Book"
-  #f.user Factory(:user)
-  #f.location Factory(:location)
+  f.sequence(:title) { |n| "Book #{n}" }
+  f.association :user
+  f.association :location
   f.isbn "1234567890"
 end
 
 Factory.define :location do |f|
-  f.name "Location"
-  #f.user Factory(:user)
+  f.sequence(:name) { |n| "Location #{n}" }
+  f.association :user
 end
 
 Factory.define :request do |f|
+  f.association :user
+  f.association :book
 end
